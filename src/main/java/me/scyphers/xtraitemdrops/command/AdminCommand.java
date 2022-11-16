7b27f74ef64db37fbb3476856a292f9a1f4520cc
Scyphers <scyphers.dev@gmail.com>
2022-11-16 18:40:00 +0800

    Implement admin command with subcommands

--- src/main/java/me/scyphers/xtraitemdrops/command/AdminCommand.java
+++ src/main/java/me/scyphers/xtraitemdrops/command/AdminCommand.java
@@ -1,24 +1,46 @@
 package me.scyphers.xtraitemdrops.command;
 
-import org.bukkit.command.Command;
+import me.scyphers.xtraitemdrops.XtraItemDrops;
+import me.scyphers.xtraitemdrops.command.admin.UICommand;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.TabExecutor;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.entity.Player;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.Objects;
 
-public class AdminCommand implements TabExecutor {
+public class AdminCommand extends SubCommandMap<XtraItemDrops> {
 
+    public AdminCommand(XtraItemDrops plugin) {
+        super(plugin, "xtraitemdrops.commands.admin");
+        addSubcommand("open", new UICommand(plugin));
 
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return false;
+        PluginCommand command = Objects.requireNonNull(plugin.getCommand("drops"));
+        command.setExecutor(this);
+        command.setTabCompleter(this);
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return Collections.emptyList();
+    public void sendNoPermission(CommandSender sender) {
+        // TODO - messaging system
+        sender.sendMessage("no permission");
+    }
+
+    @Override
+    public void sendUnknownCommand(CommandSender sender) {
+        // TODO - messaging system
+        sender.sendMessage("unknown command");
+    }
+
+    @Override
+    public boolean onNoArgCommand(CommandSender sender) {
+
+        if (!(sender instanceof Player player)) {
+            // TODO - messaging system
+            sender.sendMessage("must be player");
+            return true;
+        }
+
+        // execute the command, bypassing permission checks
+        return getCommand("open").onCommand(sender, new String[] {"home", player.getName(), player.getName()});
     }
 }
