b0ae1f849401c429a9cf8995d2b78bcb72ecc952
Scyphers <scyphers.dev@gmail.com>
2022-11-16 18:36:58 +0800

    Add queue to plugin

--- src/main/java/me/scyphers/xtraitemdrops/XtraItemDrops.java
+++ src/main/java/me/scyphers/xtraitemdrops/XtraItemDrops.java
@@ -4,6 +4,14 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class XtraItemDrops extends JavaPlugin {
 
+    private AsyncQueue<XtraItemDrops> asyncQueue;
 
+    @Override
+    public void onEnable() {
+        this.asyncQueue = new AsyncQueue<>(this, 20);
+    }
 
+    public AsyncQueue<XtraItemDrops> getQueue() {
+        return asyncQueue;
+    }
 }
