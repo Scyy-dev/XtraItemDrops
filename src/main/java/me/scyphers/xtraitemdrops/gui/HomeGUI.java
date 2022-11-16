3fdd7faf0590c8ca779e41c5e362245d0645c8b9
Scyphers <scyphers.dev@gmail.com>
2022-11-16 17:33:02 +0800

    Add first GUI

---
+++ src/main/java/me/scyphers/xtraitemdrops/gui/HomeGUI.java
@@ -0,0 +1,38 @@
+package me.scyphers.xtraitemdrops.gui;
+
+import me.scyphers.xtraitemdrops.ui.GUI;
+import me.scyphers.xtraitemdrops.ui.ItemBuilder;
+import me.scyphers.xtraitemdrops.ui.MenuGUI;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
+
+public class HomeGUI extends MenuGUI {
+
+    private static final String TITLE = "<dark_purple>XtraItemDrops</dark_purple>";
+
+    public HomeGUI(@NotNull Plugin plugin, @NotNull Player player, UUID intendedViewer) {
+        super(plugin, player, intendedViewer, TITLE, 54);
+    }
+
+    @Override
+    public void draw() {
+        setItem(29, new ItemBuilder(Material.CREEPER_HEAD).name("<dark_purple>Entities</dark_purple>").build());
+        setItem(33, new ItemBuilder(Material.GRASS_BLOCK).name("<dark_purple>Blocks</dark_purple>").build());
+    }
+
+    @Override
+    public GUI<?> onClick(int slot, ClickType type) {
+        // TODO - add paged GUIs for drops
+        return null;
+    }
+
+    @Override
+    public GUI<?> getPreviousGUI() {
+        return this;
+    }
+}
